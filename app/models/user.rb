0b387abac0efb0ebfcce1824a22239e1b4d2223f
Anastasia <rubyspiderrobot@gmail.com>
2018-01-10 11:36:12 +0000

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,8 @@ class User < ActiveRecord::Base
   validates :email, presence: true, 
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-    validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
+  
+  
+  
 end
