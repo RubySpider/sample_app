177802da897750d7e544af5c91ec5fb6cee22fef
Anastasiya Kolotova <parvus.bombus@gmail.com>
2018-01-13 15:14:58 +0000

    Finish sign in

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,10 +1,20 @@
 include ApplicationHelper
 
-# def full_title(page_title)
-#   base_title = "Ruby on Rails Tutorial Sample App"
-#   if page_title.empty?
-#     base_title
-#   else
-#     "#{base_title} | #{page_title}"
-#   end
-# end
+def fields_valid_user_information
+  fill_in "Name",         with: "Example User"
+  fill_in "Email",        with: "user@example.com"
+  fill_in "Password",     with: "foobar"
+  fill_in "Confirmation", with: "foobar"
+end
+
+def valid_signin(user)
+  fill_in "Email",    with: user.email.upcase
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-error', text: message)
+  end
+end
