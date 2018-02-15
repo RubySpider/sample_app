f068e6638fa66c96d6cc43293d5bd7104b6e5f10
Anastasiya Kolotova <parvus.bombus@gmail.com>
2018-02-15 17:50:30 +0000

    Add user following

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180201181945) do
+ActiveRecord::Schema.define(version: 20180209170240) do
 
   create_table "microposts", force: true do |t|
     t.string   "content"
